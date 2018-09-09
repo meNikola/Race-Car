04bac1d3d81b69164e9d80b2a23c4f30557cfc60
Nikola Kovacevic <kovacvb@hotmail.com>
2018-09-09 20:14:05 +0200

    Race car app

---
+++ js/script.js
@@ -0,0 +1,67 @@
+$(function() {
+
+    $('#go').click(function() {
+
+    function checkIfComplete() {
+      if(isComplete == false) {
+        isComplete = true;
+      } else {
+        place = 'second';
+      }
+    }
+
+
+
+    var carWidth = $('#car1').width();
+    var raceTrackWidth = $(window).width() - carWidth;
+
+    var raceTime1 = Math.floor((Math.random() * 5000) + 1);
+    var raceTime2 = Math.floor((Math.random() * 5000) + 1);
+
+    var isComplete = false;
+
+    var place = 'first';
+
+
+    // animate car 1
+    $('#car1').animate({
+
+      left: raceTrackWidth
+
+    }, raceTime1, function() {
+      checkIfComplete();
+
+      $('#raceInfo1 span').text('Finished in ' +place+ ' place and clocked in at ' +raceTime1+ ' milliseconds!');
+    });
+
+
+
+    // animate car 2
+    $('#car2').animate({
+
+      left: raceTrackWidth
+
+    }, raceTime2, function() {
+      checkIfComplete();
+
+      $('#raceInfo2 span').text('Finished in ' +place+ ' place and clocked in at ' +raceTime2+ ' milliseconds!');
+    });
+
+
+  });
+
+  $('#reset').click(function() {
+    $('.car').css('left','0');
+    $('.raceInfo span').text('');
+  });
+
+
+
+
+
+
+
+
+
+
+});
